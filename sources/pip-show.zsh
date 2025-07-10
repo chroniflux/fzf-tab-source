0911a8bc9dcdaa9f458e3e37be86495eb5a0ac85
jhxu <me@bitluna.me>
2025-07-09 21:58:49 -0400

    Remove debug output from pip-show.zsh completions

--- sources/pip-show.zsh
+++ sources/pip-show.zsh
@@ -2,10 +2,10 @@
 
 case $group in
   *'command'*|'completions'|*'sub-command'*)
-    ${words[1, CURRENT-1]} "$word" --help 2>&1 | bat --language=help --plain
+    echo "补全词(word): '$word', 补全组(group): '$group'" > /tmp/docker_debug.txt
     ;;
 
   *)
-    echo "补全词(word): '$word', 补全组(group): '$group'" > /tmp/docker_debug.txt
+
     ;;
 esac
