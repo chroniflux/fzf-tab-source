4fb0f2e90078afe1a06aef2a3c2dbd070fc7d682
jhxu <me@bitluna.me>
2025-07-09 21:43:26 -0400

    Add debug logging for word and group in docker.zsh

--- sources/docker.zsh
+++ sources/docker.zsh
@@ -1,9 +1,10 @@
 # :fzf-tab:complete:(\\|*/|)docker:
 
 case $group in
-  *'sub-command'|*'command')
+  *' sub-command'|*' command')
     docker "$word" --help 2>&1 | bat --language=help --plain
     ;;
   *)
+    echo "补全词(word): '$word', 补全组(group): '$group'" > /tmp/docker_debug.txt
     ;;
 esac
