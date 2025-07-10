235ad00cb35c752a7f369414bc1287fc3ec95e15
jhxu <me@bitluna.me>
2025-07-09 21:45:27 -0400

    Remove debug logging from docker.zsh completion script

--- sources/docker.zsh
+++ sources/docker.zsh
@@ -1,10 +1,9 @@
 # :fzf-tab:complete:(\\|*/|)docker:
 
 case $group in
-  *' sub-command'|*' command')
+  'completions')
     docker "$word" --help 2>&1 | bat --language=help --plain
     ;;
   *)
-    echo "补全词(word): '$word', 补全组(group): '$group'" > /tmp/docker_debug.txt
     ;;
 esac
