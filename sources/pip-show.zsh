45e8fb41c82fff6410038f88e9686ab8a8a32c1e
jhxu <me@bitluna.me>
2025-07-09 21:54:46 -0400

    Improve pip show completion to display help with syntax highlighting

--- sources/pip-show.zsh
+++ sources/pip-show.zsh
@@ -1,2 +1,10 @@
-# :fzf-tab:complete:(\\|*/|)pip(|3*):argument-rest
-pip show $word | bat -lyaml
+# :fzf-tab:complete:(\\|*/|)pip(|3*):
+
+case $group in
+  *'command'*|'completions'|*'sub-command'*)
+    ${words[1, CURRENT-1]} "$word" --help 2>&1 | bat --language=help --plain
+    ;;
+
+  *)
+    ;;
+esac
